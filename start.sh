138c08d1d26c0d1c18f6ad868c0b4b44cc38ce4b
CosmicQ <jason@cosmicegg.net>
2020-02-25 19:41:24 -0500

    paramaterized the directory

--- start.sh
+++ start.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
+DIR="/mnt/e"
 
-if ! [ -d /mnt/e/$1 ]; then
+if ! [ -d $DIR/$1 ]; then
   echo "Making a new server directory..."
-  mkdir -p /mnt/e/$1
+  mkdir -p $DIR/$1
 fi
 
-
-if ! [ -f /mnt/e/$1/server.jar ]; then
+if ! [ -f $DIR/$1/server.jar ]; then
   echo "Getting server.jar from minecraft.net..."
   SERVER_JAR=$(curl -s https://www.minecraft.net/en-us/download/server/ |grep server.jar |awk -F\" '{print $2}')
-  wget -O /mnt/e/$1/server.jar $SERVER_JAR
+  wget -O $DIR/$1/server.jar $SERVER_JAR
 
 fi
 
-cd /mnt/e/$1
+cd $DIR/$1
 echo "Starting minecraft..."
 
-if ! grep "eula=true" /mnt/e/$1/eula.txt; then
-  echo "eula=true" > /mnt/e/$1/eula.txt
+if ! grep "eula=true" $DIR/$1/eula.txt; then
+  echo "eula=true" > $DIR/$1/eula.txt
 fi
 
-/mnt/e/jdk1.8.0_231/bin/java -Xmx8024M -Xms1024M -XX:+UseG1GC -jar server.jar nogui
+$DIR/jdk1.8.0_231/bin/java -Xmx8024M -Xms1024M -XX:+UseG1GC -jar server.jar nogui
