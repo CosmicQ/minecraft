b18e56c99e4307aaf8deace66ee3eb4f460e669a
CosmicQ <jason@cosmicegg.net>
2020-02-25 19:45:09 -0500

    Paramaterized the java dir

--- start.sh
+++ start.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-DIR="/mnt/e"
+DATA_DIR="/mnt/e"
+JAVA_DIR="$DATA_DIR/jdk1.8.0_231/bin"
 
-if ! [ -d $DIR/$1 ]; then
+if ! [ -d $DATA_DIR/$1 ]; then
   echo "Making a new server directory..."
-  mkdir -p $DIR/$1
+  mkdir -p $DATA_DIR/$1
 fi
 
-if ! [ -f $DIR/$1/server.jar ]; then
+if ! [ -f $DATA_DIR/$1/server.jar ]; then
   echo "Getting server.jar from minecraft.net..."
   SERVER_JAR=$(curl -s https://www.minecraft.net/en-us/download/server/ |grep server.jar |awk -F\" '{print $2}')
-  wget -O $DIR/$1/server.jar $SERVER_JAR
+  wget -O $DATA_DIR/$1/server.jar $SERVER_JAR
 
 fi
 
-cd $DIR/$1
+cd $DATA_DIR/$1
 echo "Starting minecraft..."
 
-if ! grep "eula=true" $DIR/$1/eula.txt; then
-  echo "eula=true" > $DIR/$1/eula.txt
+if ! grep "eula=true" $DATA_DIR/$1/eula.txt; then
+  echo "eula=true" > $DATA_DIR/$1/eula.txt
 fi
 
-$DIR/jdk1.8.0_231/bin/java -Xmx8024M -Xms1024M -XX:+UseG1GC -jar server.jar nogui
+$JAVA_DIR/java -Xmx8024M -Xms1024M -XX:+UseG1GC -jar server.jar nogui
